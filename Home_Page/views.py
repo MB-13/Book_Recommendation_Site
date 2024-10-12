af54e82a4e875807240bca05c8cea20065eff6c3
MB-13 <mahavir13042003@gmail.com>
2024-10-12 15:14:06 +0530

    change

--- Home_Page/views.py
+++ Home_Page/views.py
@@ -3,6 +3,6 @@ from django.views import View
 
 # Create your views here.
 class HomeView(View):
-    def get(self,request):
+    def post(self,request):
         return render(request,"Home_Page/home.html")
     
