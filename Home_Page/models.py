4ec7ceabf5a9c2e5d6f932aa002e8de30023c421
MB-13 <mahavir13042003@gmail.com>
2024-10-21 19:49:55 +0530

    change

--- Home_Page/models.py
+++ Home_Page/models.py
@@ -21,8 +21,17 @@ class Book_Detail(models.Model):
     Description = models.CharField(max_length=8000,null=True)
     Publish_year = models.CharField(max_length=20,null=True)
     Cover_url = models.CharField(max_length=300,null=True)
-    Rating = models.IntegerField(null=True,default=0,validators=[MinValueValidator(0),MaxValueValidator(5)])
+    Rating = models.FloatField(null=True,default=0,validators=[MinValueValidator(0),MaxValueValidator(5)])
     Rating_Count = models.BigIntegerField(null=True,default=0,validators=[MinValueValidator(0)])
+    weighted_avg = models.FloatField(null=True)
+    
+    
+    nor_mean = 90523
+    rate_mean = 3.25
     
     def __str__(self):
         return f"{self.Title}"
+    
+    def save(self,*args, **kwargs):
+        self.weighted_avg = ((self.Rating * self.Rating_Count) + (self.nor_mean*self.rate_mean)) / (self.Rating_Count + self.nor_mean)
+        super(Book_Detail,self).save(*args, **kwargs)
